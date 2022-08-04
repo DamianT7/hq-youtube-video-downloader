8325526b76098201f82cfa785cfa1bac46b6c99e
Damian Teske <damiant7@live.com>
2022-08-04 15:27:33 +0200

    initial - code

---
+++ hq-youtube-video-downloader/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using hq_youtube_video_downloader.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace hq_youtube_video_downloader.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ hq-youtube-video-downloader/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hq_youtube_video_downloader.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
